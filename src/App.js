59e5a205af79dcd512a9e8602d0c8361e55676ad
princeparvat80 <princeparvat80@gmail.com>
2021-06-06 23:44:49 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from "./Components/Todo";
-
 function App () {
   return (
     <div>
       <h1>My To-Dos</h1>
-      <Todo text="React Beginner"/>
-      <Todo text="React Intermidiate"/>
-      <Todo text="React Master"/>
     </div>
   )
 }
--- src/Components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-function Backdrop(props){
-    return(
-        <div className="backdrop" onClick={props.onPress}></div>
-    );
-}
-
-export default Backdrop;
