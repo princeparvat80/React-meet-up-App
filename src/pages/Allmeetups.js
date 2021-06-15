94e9771bb0dbad8711fc18837cc2868857128785
princeparvat80 <princeparvat80@gmail.com>
2021-06-15 14:41:08 +0530

    All meetups

--- src/pages/Allmeetups.js
+++ src/pages/Allmeetups.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import MeetupItem from '../Components/meetups/MeetupItem';
+import MeetupList from '../Components/meetups/MeetupList';
+const DUMMY_DATA = [
+    {
+      id: 'm1',
+      title: 'This is a first meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+    {
+      id: 'm2',
+      title: 'This is a second meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+  ];
 
 function AllmeetupsPages() {
     return (
-        <div>
-           AllmeetupsPages
-        </div>
+        <section>
+          <h1>Meetups</h1>
+            <ul>
+                {/* {DUMMY_DATA.map((meetup)=> {
+                    return<li key={meetup.id}>{meetup.title}</li>
+                })} */}
+                <MeetupList item = {DUMMY_DATA}/>
+
+            </ul>
+        </section>
     )
 }
 
